dd17f6c0b0f0162bdfdf7cabf68d08d45303582b
Elliot662 <elliotmeltzer@hotmail.co.uk>
2022-11-30 12:12:15 +0000

    new api

--- src/App.js
+++ src/App.js
@@ -1,39 +1,48 @@
 import './App.css';
 import { useEffect, useState } from "react"
-import Game from game.js
-import Welcome from welcome.jss
+import Welcome from './welcome';
+import ID from './Id';
+import Artefact from "./Artefact"
 
 const App = () => {
+    const [data, setData] = useState("")
     const [error, setError] = useState(null)
-    const [item, setItem] = useState("")
-    useEffect(() => {
-        const getter = async () => {
-            try {
-                let response = await fetch('https://www.mmobomb.com/api1/games/?limits=10')
-                if (!response.ok) {
-                    throw new Error(response.statusText)
-                }
-                let newItem = await response.json()
-                setItem(newItem);
-            } catch (error) {
-                setError("Error could not fetch the data")
-                console.log(error.message)
-            }
+    const [userInput, setUserInput] = useState("546303")
+
+    const getter = async () => {
+        try {
+        let response = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${userInput}`)
+        if(!response.ok){
+            throw new Error(response.statusText)
         }
+        let newData = await response.json()
+        setData(newData)
+        } catch(error){
+            setError("could not fetch data")
+            console.log(error.message)
+        }
+    }
+    
+    useEffect(() => {
         getter()
     }, [])
-    if (!item) {
-        return <h1>The page is loading...</h1>
+    if(!data){
+        return <h1>loading...</h1>
     }
-    if (error) {
-        return <h1>There has been an error</h1>
+    if(error){
+        return <h1>Their has been an error sorry</h1>
     }
-    return (
-        <div className='App'>
-            {item.map((game) => {
-                <div><Game item={game} /></div>
-                })}
-
+    return(
+        <div>
+            <Welcome/>
+            <ID/>
+            <input 
+                type="text"
+                value={userInput}
+                onChange={(event) => setUserInput(event.target.value)}
+            />
+            <button onClick={getter}>Enter</button>
+            <Artefact data={data}/>
         </div>
     )
 }
--- src/welcome.js
+++ src/welcome.js
@@ -1,13 +1,11 @@
 const Welcome = () => {
     return(
         <div class="center">
-            <h1> Game Changer </h1>
-            <p> Hi & welcome to Game Changer!</p>
-            <p> Come on in and explore the world of games where all consoles come to life!</p>
+            <h1> Artefact Changer </h1>
+            <p> Hi & welcome to Artefact Changer!</p>
+            <p> Come on in and explore the world of artefacts where all artefacts come to life!</p>
         </div>
     )
 }
-
-
 
 export default Welcome
